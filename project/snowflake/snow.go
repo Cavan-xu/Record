be39235f537e4c6e455b53fb935c6eaf764a44d6
cavan.xu <2582099310@qq.com>
2022-06-19 10:10:37 +0800

    feature: snow flake modify

--- project/snowflake/snow.go
+++ project/snowflake/snow.go
@@ -1,5 +1,16 @@
 package snowflake
 
+// GenerateId 用于分布式场景下生成唯一 id
+/*
+  最高位是符号位，始终为 0，不可用。
+  41 位的时间序列，精确到毫秒级，41位的长度可以使用69年。时间位还有一个很重要的作用是可以根据时间进行排序。
+  10 位的机器标识，10位的长度最多支持部署1023个节点。
+  12 位的计数序列号，序列号即一系列的自增id，可以支持同一节点同一毫秒生成多个ID序号，12位的计数序列号支持每个节点每毫秒产生4095个ID序号。
+
+  符号位		41bit 时间戳									十位机器编号		 12位计数序号
+  0 		00000000000000000000000000000000000000000	0000000000		000000000000
+*/
+
 import (
 	"sync"
 	"time"
@@ -9,6 +20,8 @@ const (
 	workerBit uint8 = 10 // 机器位数
 	stepBit   uint8 = 12 // 计数位数
 	maxStep   int64 = -1 ^ (-1 << stepBit)
+
+	milliSecond = int64(time.Millisecond)
 
 	timeShift = workerBit + stepBit // 	时间左移位数
 )
@@ -35,19 +48,13 @@ type SnowGenerator struct {
 }
 
 // New SnowGenerator
-func New(node ...int64) *SnowGenerator {
+func New(node int64) *SnowGenerator {
 	once.Do(func() {
-		if len(node) == 0 {
-			panic("initialization SnowGenerator lack param [\"node\"]")
-		}
-		startTime, err := time.ParseInLocation(time.RFC3339, "2022-06-18T20:06:05+08:00", time.Local)
-		if err != nil {
-			panic(err)
-		}
-		epoch := startTime.UnixMilli()
+		startTime, _ := time.ParseInLocation(time.RFC3339, "2022-06-18T20:06:05+08:00", time.Local)
+		epoch := startTime.UnixNano() / milliSecond
 		instance = &SnowGenerator{
 			epoch: epoch,
-			node:  node[0],
+			node:  node,
 			time:  0,
 			step:  0,
 		}
@@ -55,27 +62,17 @@ func New(node ...int64) *SnowGenerator {
 	return instance
 }
 
-// GenerateId 用于分布式场景下生成唯一 id
-/*
-  最高位是符号位，始终为 0，不可用。
-  41 位的时间序列，精确到毫秒级，41位的长度可以使用69年。时间位还有一个很重要的作用是可以根据时间进行排序。
-  10 位的机器标识，10位的长度最多支持部署1023个节点。
-  12 位的计数序列号，序列号即一系列的自增id，可以支持同一节点同一毫秒生成多个ID序号，12位的计数序列号支持每个节点每毫秒产生4095个ID序号。
-
-  符号位		41bit 时间戳									十位机器编号		 12位计数序号
-  0 		00000000000000000000000000000000000000000	0000000000		000000000000
-*/
 func (sg *SnowGenerator) GenerateId() ID {
 	sg.mu.Lock()
 	defer sg.mu.Unlock()
 
-	now := time.Now().UnixMilli()
+	now := time.Now().UnixNano() / milliSecond
 
 	if sg.time == now { // 在同一个毫秒里面
 		sg.step = (sg.step + 1) & maxStep
 		if sg.step == 0 {
 			for now <= sg.time {
-				now = time.Now().UnixMilli()
+				now = time.Now().UnixNano() / milliSecond
 			}
 		}
 	} else {
--- project/snowflake/snow_test.go
+++ project/snowflake/snow_test.go
@@ -6,5 +6,4 @@ import (
 
 func TestGenerateId(t *testing.T) {
 	t.Log(New(1).GenerateId().Int64())
-	t.Log(New().GenerateId().Int64())
 }
