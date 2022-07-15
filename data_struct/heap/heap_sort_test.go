c0fa0edb3d956d12057badcc530a46b46e9accc7
cavan.xu <3395058394@qq.com>
2022-07-15 18:13:00 +0800

    feature: heap

--- data_struct/heap/heap_sort_test.go
+++ data_struct/heap/heap_sort_test.go
@@ -14,8 +14,8 @@ func TestSort(t *testing.T) {
 }
 
 func TestLocalSort(t *testing.T) {
-	arr := []int32{9, 8, 7, 6, 4, 3, 2, 1}
-	want := []int32{1, 2, 3, 4, 6, 7, 8, 9}
+	arr := []int32{6, 3, 8, 2, 4, 7, 9, 1}
+	want := []int32{9, 8, 7, 6, 4, 3, 2, 1}
 
 	assert.Equal(t, want, LocalSort(arr))
 }
